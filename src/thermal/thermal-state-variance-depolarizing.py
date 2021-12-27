a1a7be62180657b5e93d20f1cfb85b33b976984f
Wikstahl <pontus.wikstahl@gmail.com>
2021-12-27 14:35:32 +0100

    Changed to code to python instead of notebook.

---
+++ src/thermal/thermal-state-variance-depolarizing.py
@@ -0,0 +1,31 @@
+import pickle
+import numpy as np
+import networkx as nx
+from module import DepolarizingChannel, MyClass
+
+# Initialize dictionary
+data = dict()
+# Loop over all graphs
+for idx in range(30):
+    # Path
+    path = "../../data/max_cut_" + str(idx) + "/"
+    # Load graph
+    graph = nx.readwrite.gpickle.read_gpickle(path + "graph")
+    # Create object
+    obj = MyClass(graph)
+    # Create mixed state
+    rho_in = np.diag(obj.mixed_state())
+    # Loop over noise levels
+    for key, p in enumerate(np.linspace(0, .5, 26)):
+        # Simulate virtual distillation
+        rho_out = obj.simulate_virtual_distillation(
+            rho_in,
+            with_noise=DepolarizingChannel(p=p)
+        )
+        # Calculate mitigated variance
+        mv = obj.mitigated_variance(rho_out)
+        data[str(key)] = tuple([p, mv])
+    # Save results
+    filename = path + "thermal_variance_depolarizing"
+    with open(filename, 'wb') as f:
+        pickle.dump(data, f)
