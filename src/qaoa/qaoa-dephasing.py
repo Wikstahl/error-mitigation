9a4edd36eef09aeaaff62ec7488125630283435c
Wikstahl <pontus.wikstahl@gmail.com>
2022-09-13 09:20:06 -0400

    removed polish function

--- src/qaoa/qaoa-dephasing.py
+++ src/qaoa/qaoa-dephasing.py
@@ -9,24 +9,24 @@ data = dict()
 # Loop over all instances
 for idx in range(30):
     # Path
-    path = "../../data/max_cut_"+str(idx)+"/"
+    path = "../../data/max_cut_" + str(idx) + "/"
     # Load graph
     graph = nx.readwrite.gpickle.read_gpickle(path + "graph")
     # Create object
     obj = MyClass(graph)
     # Loop over noise levels
-    for key, p in enumerate(np.linspace(0,0.1,21)):
+    for key, p in enumerate(np.linspace(0, 0.1, 21)):
         # Args to brute
         args = ([DephasingChannel(p=p)])
         # Optimization bounds
-        ranges = ((0, np.pi), (0, np.pi/2))
+        ranges = ((0, np.pi), (0, np.pi / 2))
         # Brute force on 100 x 100 grid
         if p == 0:
             res = brute(obj.optimize_qaoa_with_vd, ranges, args=None, Ns=100,
-                        full_output=True, finish=fmin, workers=6)
+                        full_output=True, finish=None, workers=6)
         else:
             res = brute(obj.optimize_qaoa_with_vd, ranges, args=args, Ns=100,
-                        full_output=True, finish=fmin, workers=6)
+                        full_output=True, finish=None, workers=6)
         data[str(key)] = (p, res)
     # Save results
     filename = path + "qaoa_parameters_brute_dephasing_with_vd"
